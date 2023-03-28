0eeb36685c54fbd659d58f9a246205e57c678966
ysunk <ysunk@edu.dju.ac.kr>
2023-03-28 22:36:35 +0900

    캐릭터 2주차
    1. 고양이
    2. 강아지

---
+++ 캐릭터 2-1주차2/2-1-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( O )\"\"\"\\\n");
+	printf("|\"\^\"\`    |\n");
+	printf("||_/=\\\\__|\n");
+
+	return 0;
+}
